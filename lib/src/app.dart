f0317d668b5fc0d9674b9913065d037c015af15f
Andre LFA <andrelfaareas@gmail.com>
2019-03-19 18:27:53 -0300

    create function to retrive data from the API

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,32 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'models/image_model.dart';
+import 'dart:convert';
 
 class App extends StatefulWidget {
   createState() {
     return AppState();
   }
-
 }
 
 class AppState extends State<App> {
   int count = 0;
+
+  void fetchImage() async {
+    count++;
+    var response = await get('https://jsonplaceholder.typicode.com/photos/$count');
+    var imageModel = new ImageModel.fromJson(json.decode(response.body));
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    home: Scaffold(
-      body: Text('$count'),
-      appBar: AppBar(
-        title: Text("Let's see some images!"),
+      home: Scaffold(
+        body: Text('$count'),
+        appBar: AppBar(
+          title: Text("Let's see some images!"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: fetchImage,
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          setState(() {
-            count++;  
-          });
-          },
-      ),
-    ),
-  );
+    );
   }
-  
 }
